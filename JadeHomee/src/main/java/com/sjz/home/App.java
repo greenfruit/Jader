71a4a3c68f1ac1b11a72f093f6b23d0cf17c9778
zhangshouzheng <zhangshouzheng@le.com>
2017-03-09 16:23:16 +0800

    init

---
+++ JadeHomee/src/main/java/com/sjz/home/App.java
@@ -0,0 +1,13 @@
+package com.sjz.home;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
